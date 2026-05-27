ad1172e5d20ccdc28063f5884a4c4e269fed5991
Xuan Hao <material.energitics@gmail.com>
2026-05-27 18:16:56 -0400

    images + mass image comparison

--- main.py
+++ main.py
@@ -4,76 +4,6 @@ from numpy.linalg import eigh
 import imageio.v3 as iio
 from PIL import Image, ImageOps
 
-def svd(m, dim):
-    u, e, v_t = np.linalg.svd(m, full_matrices=False)
-
-    out = np.zeros(m.shape)
-    for i in range(min(dim, min(m.shape))):
-        out += np.outer(u[:,i], v_t[i]) * e[i]
-    
-    return out
-
-def rgbToImg(r, g, b):
-    shape = r.shape + (3,)
-    img = np.zeros(shape)
-    print(img.shape)
-    for i in range(r.shape[0]):
-        for j in range(r.shape[1]):
-            img[i][j][0] = min(max(r[i][j], 0), 255)
-            img[i][j][1] = min(max(g[i][j], 0), 255)
-            img[i][j][2] = min(max(b[i][j], 0), 255)
-    return img
-
-def svdColored(img, dim):
-    r = svd(img[:,:,0], dim)
-    g = svd(img[:,:,1], dim)
-    b = svd(img[:,:,2], dim)
-
-    return rgbToImg(r, g, b).astype(np.uint8)
-
-def get_norm(svd):
-
-    return (norm_r + norm_g + norm_b) / 3
-
-def get_norm_r(svd):
-    size = svd.shape[0] * svd.shape[1]
-    norm_r = np.linalg.matrix_norm(svd[:,:,0]) / size
-    return norm_r
-
-def get_norm_g(svd):
-    size = svd.shape[0] * svd.shape[1]
-    norm_g = np.linalg.matrix_norm(svd[:,:,1]) / size
-    return norm_g
-
-def get_norm_b(svd):
-    size = svd.shape[0] * svd.shape[1]
-    norm_b = np.linalg.matrix_norm(svd[:,:,2]) / size
-    return norm_b
-
-# experimentation with colored images
-
-# img_og = iio.imread("orange_og.png")
-# img_og_svd = svdColored(img_og, 2)
-
-# iio.imwrite("out_og.png", img_og_svd)
-
-# img_1 = iio.imread("orange1.png")
-# img_1_svd = svdColored(img_1, 2)
-
-# iio.imwrite("out_1.png", img_1_svd)
-
-# img_apple = iio.imread("apple.png")
-# img_apple_svd = svdColored(img_apple, 2)
-
-# iio.imwrite("out_apple.png", img_apple_svd)
-
-# norm_og = get_norm(img_og_svd)
-# norm_img_1 = get_norm(img_1_svd)
-# norm_img_apple = get_norm(img_apple)
-
-# print(norm_img_1 - norm_og)
-# print(norm_img_apple - norm_og)
-
 def img_to_vector(img):
     matrix = np.array(img)
     out = []
@@ -88,21 +18,21 @@ def import_img(src):
     return ImageOps.grayscale(img)
 
 def resize(img):
-    return img.resize((512, 512), Image.BILINEAR)
+    return img.resize((64, 64), Image.BILINEAR)
 
 def cosine(vec_1, vec_2):
     return (np.dot(vec_1, vec_2)) / (np.sqrt(vec_1.dot(vec_1)) * np.sqrt(vec_2.dot(vec_2)))
 
 
-orange_og = import_img("photos/orange.png")
-orange_og = resize(orange_og)
-iio.imwrite("out_og.png", np.array(orange_og).astype(np.uint8))
-vec_og = img_to_vector(orange_og)
+orange = import_img("photos/orange.png")
+orange = resize(orange)
+iio.imwrite("out_orange.png", np.array(orange).astype(np.uint8))
+vec_orange = img_to_vector(orange)
 
-orange1 = import_img("photos/tangerine.png")
-orange1 = resize(orange1)
-iio.imwrite("out_1.png", np.array(orange1).astype(np.uint8))
-vec_orange = img_to_vector(orange1)
+tangerine = import_img("photos/tangerine.png")
+tangerine = resize(tangerine)
+iio.imwrite("out_tangerine.png", np.array(tangerine).astype(np.uint8))
+vec_tangerine = img_to_vector(tangerine)
 
 apple = import_img("photos/apple.png")
 apple = resize(apple)
@@ -110,10 +40,10 @@ iio.imwrite("out_apple.png", np.array(apple).astype(np.uint8))
 vec_apple = img_to_vector(apple)
 
 print("Cosine of orange and tangerine: ")
-print(cosine(vec_og, vec_orange))
+print(cosine(vec_orange, vec_tangerine))
 
 print("Cosine of orange and apple: ")
-print(cosine(vec_og, vec_apple))
+print(cosine(vec_orange, vec_apple))
 
 
 ex_1 = import_img("example_1.png")
@@ -133,3 +63,50 @@ r_col = np.array( # 3 x 6
 out = r_row @ ex_1 @ r_col.T
 
 iio.imwrite("ex_out.png", out.astype(np.uint8))
+
+lemon = import_img("photos/lemon.png")
+lemon = resize(lemon)
+iio.imwrite("out_lemon.png", np.array(lemon).astype(np.uint8))
+vec_lemon = img_to_vector(lemon)
+
+lime = import_img("photos/lime.jpg")
+lime = resize(lime)
+iio.imwrite("out_lime.png", np.array(lime).astype(np.uint8))
+vec_lime = img_to_vector(lime)
+
+grape = import_img("photos/grape.jpg")
+grape = resize(grape)
+iio.imwrite("out_grape.png", np.array(grape).astype(np.uint8))
+vec_grape = img_to_vector(grape)
+
+def normalize(vec):
+    return vec / np.linalg.norm(vec)
+
+images = np.array([
+    normalize(vec_tangerine),
+    normalize(vec_orange),
+    normalize(vec_apple),
+    normalize(vec_lemon),
+    normalize(vec_lime),
+    normalize(vec_grape)
+])
+
+
+
+test_image = import_img("photos/test.jpg")
+test_image = resize(test_image)
+iio.imwrite("out_test.png", np.array(test_image).astype(np.uint8))
+vec_test = normalize(img_to_vector(test_image))
+
+output = (images @ vec_test)
+print("Similarity of Test Image: ")
+print(output)
+
+ranked = np.argsort(output)[::-1]
+labels = ['Tangerine', 'Orange', 'Apple', 'Lemon', 'Lime', 'Grape']
+for i in ranked:
+    print(f"  {labels[i]}: {output[i]:.3f}")
+
+
+## SVD time
+
